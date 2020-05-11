99c637c491d97eb26d36e5eb06c0d39b03f32d7d
Natasza Tereshchenko <tereshchenko.n@gmail.com>
2020-05-11 22:26:16 +0200

    Zadanie 3 i 4

---
+++ addressbook-web-tests/AccountData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class AccountData
+    {
+        private string username;
+        private string password;
+
+        //konstruktor
+        public AccountData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get
+            { 
+                return username;
+            }
+            set 
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get 
+            {
+                return password;
+            }
+            set 
+            {
+                password = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+
+        private string firstname;
+        private string middlename;
+        private string lastname;
+        private string nickname;
+        private string home;
+        private string mobile;
+        private string email;
+
+        public ContactData(string firstname)
+        {
+            this.firstname = firstname;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Middlename
+        {
+            get 
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+
+        public string Home
+        {
+            get
+            {
+                return home;
+            }
+            set
+            {
+                home = value;
+            }
+        }
+
+        public string Mobile
+        {
+            get
+            {
+                return mobile;
+            }
+            set
+            {
+                mobile = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
